7eeffd207443aa00107327ccacad9c498c9891e3
joshuahunt10 <joshua.hunt88@gmail.com>
2017-06-11 13:41:18 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+
+var displayArray= [];
+var holdArray = [];
+let eleValue = document.querySelectorAll('.num')
+let opValue = document.querySelectorAll('.operator')
+let equals = document.querySelector('#equals')
+let clear = document.querySelector('.clear-button')
+let displayBox = document.querySelector('.display-box');
+let calcAll = document.querySelector('.calculator')
+var answer;
+
+for(let i = 0; i < eleValue.length; i++){
+    eleValue[i].addEventListener('click', function(){
+      holdArray.push(eleValue[i].id);
+      displayArray = holdArray.join("");
+      displayBox.textContent = displayArray;
+    })
+}
+
+for(let i = 0; i < opValue.length; i++){
+  opValue[i].addEventListener('click', function(){
+    console.log('operator worked!');
+    holdArray.push(opValue[i].id);
+    displayArray = holdArray.join("");
+    console.log(displayArray);
+    displayBox.textContent = displayArray;
+  })
+}
+
+equals.addEventListener('click', function(){
+  console.log(displayArray);
+  noCommas = holdArray.join("");
+  answer = parseFloat(eval(noCommas).toFixed(9));
+  console.log('the answer is ' + answer);
+  displayBox.textContent = answer;
+  displayArray = [];
+  holdArray = [];
+
+})
+
+clear.addEventListener('click', function(){
+  displayArray = [];
+  holdArray = [];
+  console.log('Cleared the displayArray ' + displayArray);
+  displayBox.textContent = displayArray;
+})
